913d3efd8d661a6bf28bfa71c86af1de38a86b8d
Steppkestift <heiko.linke@posteo.de>
2015-01-11 18:42:48 +0100

    Initial commit

---
+++ src/food/Apple.java
@@ -0,0 +1,7 @@
+import food.*;
+/**
+ * Created by heiko-linke on 11.01.15.
+ */
+public class Apple extends Fruit {
+    /* any code you want */
+}
---
+++ src/food/Fruit.java
@@ -0,0 +1,7 @@
+package food;
+/**
+ * Created by heiko-linke on 11.01.15.
+ */
+public abstract class Fruit {
+    /* any code you want */
+}
